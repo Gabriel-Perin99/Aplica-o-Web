f6a8f7a8bfaf20c07b85a32b6351718751b86380
Gabriel Perin <works.gp99@gmail.com>
2024-05-14 19:57:21 -0300

    update code and content

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function calImc(){
     //pegando os valores dos inputs
     let altura = document.querySelector("#altura").value;
     let peso = document.querySelector("#peso").value;
+    
+    
+    
 
     if(altura == "" || peso == ""){
         alert("Preencha todos os campos!");
@@ -18,23 +21,45 @@ function calImc(){
             var imc = peso / (altura * altura);
             imc = parseFloat(imc.toFixed(0));//arredondado com duas casas decimais
         }
+
+    // crriando uma variavel para pegar as informações que serão dispostas em outro ducmento
+    let resultadoID = '';
+        
+        
           
             /* Mostrando o IMC na tela */
     
     if (imc <=  18.5){
-        resultado = "Abaixo do Peso";  
-    } else if (imc > 18.5 && imc <=  23.9) {
-        resultado = "Peso Normal";          
-    } else if (imc > 23.9 && imc <=  24.9) {
-        resultado = "Levemente Acima do Peso";                  
-    } else if (imc > 24.9 && imc <=    29.9) {
-        resultado = "Obesidade Grau  I";                             
-    } else if (imc > 29.9 && imc <=        34.9) {
-        resultado = "Obesidade Grau II";                            
+        resultadoID = "abaixoPeso";  
+    } else if (imc > 18.5 && imc <= 24.9) {
+        resultadoID = "normal";          
+    } else if (imc > 25 && imc <= 29.9) {
+        resultadoID = "sPeso";                  
+    } else if (imc > 30 && imc <= 34.9) {
+        resultadoID = "grauI";                             
+    } else if (imc > 35 && imc <= 39.9) {
+        resultadoID = "grauII";                            
     } else {
-        resultado = "Obesidade Morbida";
+        resultadoID = "morb";
     }
-    document.querySelector("#resultado").innerHTML="Seu IMC é: "+imc+". Classificação: "+resultado+"!";
+    
+    //variavel que linka o documento
+    const url = 'partials/infos.html';
+    //função para pegar o html
+    fetch (url)
+    .then(response => response.text())
+    .then(data =>{
+        //DOMparser é usado para analisar o HTML
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+        // Variaveis que puxam os resultados do Java e passa eles para o html
+        const contentInfo = doc.getElementById(resultadoID);
+        const resultadoHTML = document.getElementById('resultado');
+
+        if (contentInfo){
+            resultadoHTML.innerHTML = 'Seu IMC é ' + imc + ', Classificado como:'+ contentInfo.textContent; 
+        } else{console.error('Não foi possivel carregar o conteudo');}
+    })
             
     
             
@@ -57,5 +82,4 @@ document.addEventListener('DOMContentLoaded', function(){
 
 })
 
-        
 
